6d8baccbd894a29760cf26f8a13f112234a6f29a
Mateusz Laski <25green28@gmail.com>
2026-06-30 17:11:54 +0200

    Initial commit: create hero page

---
+++ app/app.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+  import AppHeader from "~/components/AppHeader.vue";
+  import AppFooter from "~/components/AppFooter.vue";
+</script>
+
+<template>
+  <div class="bg-zinc-950 text-zinc-100">
+    <AppHeader/>
+    <NuxtPage/>
+    <AppFooter/>
+  </div>
+</template>
---
+++ app/components/AppHeader.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+<!--  Mateusz Laski-->
+
+</template>
+
+<style scoped>
+
+</style>
